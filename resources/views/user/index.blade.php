356fb962d69018d5415b68517aadff691160ed40
kanon <kanon@ssd-tech.io>
2022-05-06 16:43:26 +0600

    create user

---
+++ resources/views/user/index.blade.php
@@ -0,0 +1,37 @@
+@extends('layouts.main')
+
+@section('content')
+    <!-- Page Heading -->
+    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+        <h1 class="h3 mb-0 text-gray-800">User</h1>
+    </div>
+
+    <div class="card">
+        <div class="card-header">
+            <a href="{{ route('user.create') }}" class="float-right">Create</a>
+        </div>
+        <div class="card-body">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">ID</th>
+                        <th scope="col">UserName</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">Manage</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($users as $user)
+                        <tr>
+                            <td>{{$user->id}}</td>
+                            <td>{{$user->username}}</td>
+                            <td>{{$user->email}}</td>
+                            <td>#</td>
+                        </tr>
+                    @endforeach
+
+                </tbody>
+            </table>
+        </div>
+    </div>
+@endsection
